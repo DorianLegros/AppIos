5c6dd6ef32b76ece7b43883d60dbe39a00f1c356
DorianLegros <d.ratha76@hotmail.com>
2022-03-16 15:48:40 +0100

    completed shows list visual with posters

--- AppIos/TableViewCell.swift
+++ AppIos/TableViewCell.swift
@@ -26,6 +26,18 @@ class TableViewCell: UITableViewCell {
     
     func onBind(data: Show) {
         txtTitle.text = data.name;
-        txtDesc.text = data.overview;
+        txtDesc.text = data.overview == "" ? "No description." : data.overview;
+        URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://image.tmdb.org/t/p/w342/\(data.posterPath)")!)) {
+            (data, req, error) in
+            
+            do {
+                var datas = try data
+                DispatchQueue.main.async {
+                    self.showImage.image = UIImage(data: datas!);
+                }
+            } catch {
+                
+            }
+        }.resume();
     }
 }
